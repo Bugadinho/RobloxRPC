30f519def3ba0c7de5b4191ec8bccda7ba4a6b31
Bugadinho <miguelriechi_windows8@outlook.com>
2021-11-30 17:14:10 -0300

    More robust error handling

--- main.py
+++ main.py
@@ -22,79 +22,126 @@ RobloSecurity = args.security
 ThisPresence = API.Presence.PresenceAPI(roblosecurity = RobloSecurity)
 
 RPC = None
-while RPC is None:
-    try:
-        RPC = Presence(ClientID, pipe=0)
-    except:
-        print("Failed to connect to Discord, is it running?")
-        time.sleep(5)
 
 Connected = False
 StartTime = 0
 
+def InitRPC():
+    global RPC
+    RPC = Presence(ClientID, pipe=0)
+    
 def Connect():
     global Connected
+    global RPC
+
     if Connected == False:
-        Connected = True
-        RPC.connect()
-        return True
+        try:
+            RPC.connect()
+            Connected = True
+            return True
+        except:
+            RPC = None
+            return False
     return False
 def Disconnect():
     global Connected
     global StartTime
+    global RPC
+
     if Connected == True:
-        StartTime = 0
-        Connected = False
-        RPC.close()
-        return True
+        try:
+            RPC.close()
+            StartTime = 0
+            Connected = False
+            return True
+        except:
+            RPC = None
+            return False
     return False
 
 print("Started!")
 
 while True:
-    CurrentPresence = ThisPresence.GetUserPresence(UserID)
-    GameName = CurrentPresence["lastLocation"]
-    RootPlaceID = CurrentPresence["rootPlaceId"]
-    if GameName == None or GameName == "":
-        GameName = ""
-
-    Buttons = [
-        {"label": "Profile", "url": "https://www.roblox.com/users/" + str(UserID) + "/profile"}
-    ]
-    if RootPlaceID  != "" and RootPlaceID != None:
-        Buttons.append({"label": "Experience", "url": "https://www.roblox.com/games/" + str(RootPlaceID)})
+    if RPC is None:
+        try:
+            InitRPC()
+            print("Connected to Discord!")
+        except:
+            print("Failed to connect to Discord, is it running?")
     
-    if CurrentPresence["userPresenceType"] == 1 and ShowOnline == True:
-        if StartTime == 0:
-            StartTime = math.floor(time.time())
-        if Connect() == True:
-            print("User went Online!")
-        if GameName != "":
-            RPC.update(state=GameName, details="Online", large_image="player", small_image="online", start=StartTime, buttons=Buttons)
-        else:
-            RPC.update(details="Online", large_image="player", small_image="online", start=StartTime, buttons=Buttons)
-    elif CurrentPresence["userPresenceType"] == 2:
-        if StartTime == 0:
-            StartTime = math.floor(time.time())
-        if Connect() == True:
-            print("User opened the Player!")
+    CurrentPresence = None
+    try:
+        CurrentPresence = ThisPresence.GetUserPresence(UserID)
+    except:
+        print("Failed to get presence data!")
+
+    if CurrentPresence != None:
+        GameName = CurrentPresence["lastLocation"]
+        RootPlaceID = CurrentPresence["rootPlaceId"]
+        if GameName == None or GameName == "":
+            GameName = ""
+
+        Buttons = [
+            {"label": "Profile", "url": "https://www.roblox.com/users/" + str(UserID) + "/profile"}
+        ]
+        if RootPlaceID  != "" and RootPlaceID != None:
+            Buttons.append({"label": "Experience", "url": "https://www.roblox.com/games/" + str(RootPlaceID)})
+    
+        if CurrentPresence["userPresenceType"] == 1 and ShowOnline == True:
+            if StartTime == 0:
+                StartTime = math.floor(time.time())
+            if Connect() == True:
+                print("User went Online!")
+            if GameName != "":
+                if Connected == True:
+                    try:
+                        RPC.update(state=GameName, details="Online", large_image="player", small_image="online", start=StartTime, buttons=Buttons)
+                    except:
+                        Disconnect()
+            else:
+                if Connected == True:
+                    try:
+                        RPC.update(details="Online", large_image="player", small_image="online", start=StartTime, buttons=Buttons)
+                    except:
+                        Disconnect()
+        elif CurrentPresence["userPresenceType"] == 2:
+            if StartTime == 0:
+                StartTime = math.floor(time.time())
+            if Connect() == True:
+                print("User opened the Player!")
         
-        if GameName != "":
-            RPC.update(state=GameName, details="Playing", large_image="player", small_image="playing", start=StartTime, buttons=Buttons)
-        else:
-            RPC.update(details="Playing", large_image="player", small_image="playing", start=StartTime, buttons=Buttons)
-    elif CurrentPresence["userPresenceType"] == 3:
-        if StartTime == 0:
-            StartTime = math.floor(time.time())
-        if Connect() == True:
-            print("User opened Studio!")
+            if GameName != "":
+                if Connected == True:
+                    try:
+                        RPC.update(state=GameName, details="Playing", large_image="player", small_image="playing", start=StartTime, buttons=Buttons)
+                    except:
+                        Disconnect()
+            else:
+                if Connected == True:
+                    try:
+                        RPC.update(details="Playing", large_image="player", small_image="playing", start=StartTime, buttons=Buttons)
+                    except:
+                        Disconnect()
+        elif CurrentPresence["userPresenceType"] == 3:
+            if StartTime == 0:
+                StartTime = math.floor(time.time())
+            if Connect() == True:
+                print("User opened Studio!")
         
-        if GameName != "":
-            RPC.update(state=GameName, details="Building", large_image="studio", small_image="building", start=StartTime, buttons=Buttons)
+            if GameName != "":
+                if Connected == True:
+                    try:
+                        RPC.update(state=GameName, details="Building", large_image="studio", small_image="building", start=StartTime, buttons=Buttons)
+                    except:
+                        Disconnect()
+            else:
+                if Connected == True:
+                    try:
+                        RPC.update(details="Building", large_image="studio", small_image="building", start=StartTime, buttons=Buttons)
+                    except:
+                        Disconnect()
         else:
-            RPC.update(details="Building", large_image="studio", small_image="building", start=StartTime, buttons=Buttons)
-    else:
-        if Disconnect() == True:
-            print("User went Offline!")
+            if Disconnect() == True:
+                print("User went Offline!")
     
     time.sleep(RefreshPeriod)
